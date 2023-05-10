04b199573ae982773e231064a5315ef3140a7945
carolaCaracola <jorcaroli.lopez@gmail.com>
2023-05-10 21:28:20 +0200

    creacion de archivos html\js\css e imagenes

--- main.js
+++ main.js
@@ -1,27 +1,78 @@
-let totalAmount = 0;
-const items = document.querySelectorAll('.wine, .appleJuice, .kombucha, .cocaCola, .fanta, .nestea, .beer');
+const items = document.querySelectorAll('.wine, .appleJuice, .kombucha, .cocaCola, .fanta, .nestea, .beer, .milk, .water');
 
 items.forEach(item => {
   item.addEventListener('dragstart', event => {
     event.dataTransfer.setData('text/plain', item.id);
-    event.dataTransfer.effectAllowed = 'move';
+    event.dataTransfer.effectAllowed = 'copy';
   });
 });
+
 const basket = document.querySelector('.basket');
+let totalAmount = 0;
+const cartItems = [];
 
 basket.addEventListener('dragover', event => {
   event.preventDefault();
-  event.dataTransfer.dropEffect = 'move';
+  event.dataTransfer.dropEffect = 'copy';
 });
 
 basket.addEventListener('drop', event => {
   event.preventDefault();
   const itemId = event.dataTransfer.getData('text/plain');
+
+  // Obtener el precio y nombre del elemento
   const item = document.getElementById(itemId);
-  basket.appendChild(item);
-  
-  // Actualiza el precio total de la compra
-  totalAmount += 1; // por ejemplo, se puede sumar 1€ por cada elemento añadido a la cesta
+  const price = parseFloat(item.dataset.price);
+  const name = item.alt;
+
+  // Actualizar el precio total de la compra
+  totalAmount += price;
+
+  // Actualizar el elemento HTML con el nuevo precio total
   const amountBasket = document.getElementById('amountBasket');
-  amountBasket.textContent = totalAmount.toFixed(2); // muestra el precio con dos decimales
+  amountBasket.textContent = totalAmount.toFixed(2);
+
+  // Agregar el elemento al carrito
+  cartItems.push({ id: itemId, name: name, price: price });
+
+  // Mostrar los elementos en la lista del carrito
+  const cartList = document.getElementById('cartItems');
+  cartList.innerHTML = '';
+  cartItems.forEach(cartItem => {
+    const li = document.createElement('li');
+    li.textContent = `${cartItem.name} - €${cartItem.price.toFixed(2)}`;
+
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'Eliminar';
+    removeButton.addEventListener('click', () => {
+      removeCartItem(cartItem.id);
+    });
+    li.appendChild(removeButton);
+    cartList.appendChild(li);
+  });
 });
+
+function removeCartItem(itemId) {
+  // Buscar el elemento en el carrito
+  const cartItem = cartItems.find(item => item.id === itemId);
+
+  if (cartItem) {
+    // Obtener el precio del elemento a eliminar
+    const price = cartItem.price;
+
+    // Restar el precio del elemento eliminado al total de la compra
+    totalAmount -= price;
+
+    // Actualizar el elemento HTML con el nuevo precio total
+    const amountBasket = document.getElementById('amountBasket');
+    amountBasket.textContent = totalAmount.toFixed(2);
+
+    // Eliminar el elemento del carrito
+    const itemIndex = cartItems.findIndex(item => item.id === itemId);
+    cartItems.splice(itemIndex, 1);
+
+    // Eliminar el elemento de la lista del carrito
+    const cartList = document.getElementById('cartItems');
+    cartList.removeChild(cartList.childNodes[itemIndex]);
+  }
+}
